686a91c94fe62b805484f44642babd576ccf9e17
agarciagrech <agarciagrech@gmail.com>
2023-02-16 12:07:08 +0100

    plot tests

--- Pruebas_dsqi/TestIndexCalculation.m
+++ Pruebas_dsqi/TestIndexCalculation.m
@@ -1,9 +1,9 @@
 
-% [kSQI,sSQI, pSQI, cSQI] = IndexForWindows("ECGNormal.txt");
+[kSQI,sSQI, pSQI, cSQI] = IndexForWindows("ECGNormal.txt");
 
 %disp("ECGRuido.txt");
-hold on
-[kSQI,sSQI, pSQI, cSQI] = SignalIndexes("ECGNormal.txt");
+% hold on
+% [kSQI,sSQI, pSQI, cSQI] = SignalIndexes("ECGNormal.txt");
 %yline(kSQI);
 
 
