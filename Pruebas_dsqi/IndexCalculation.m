1017ff21311e6fe9dee658ebb674ca50266151f4
AbrahamOtero <abraham.otero@gmail.com>
2023-02-21 09:56:52 +0100

    Bugs en el cálculo de los índices de calidad
    Señalados con @TODO varios bugs En los cálculos de los índices de calidad. Todos ellos en el archivo IndexCalculation

--- Pruebas_dsqi/IndexCalculation.m
+++ Pruebas_dsqi/IndexCalculation.m
@@ -13,6 +13,10 @@ end
 function[rel_power] = relativePower(data)
      freqrange1 = [5,15];
      freqrange2 = [5,50];
+     %%@TODO  necesitamos hacer el cálculo en milivoltios. Ver comentarios en
+     %baseline_drift. aquí quizás no esté afectando tanto porque hay un
+     %simple cociente que elimina el factor de escala.  Pero en otra
+     %función  es esencial por el 1-p1
      p1 = bandpower(data,330,freqrange1);
      p2 =  bandpower(data,330,freqrange2);
      rel_power = p1/p2;
@@ -20,9 +24,21 @@ function[rel_power] = relativePower(data)
 end
 
 function[var_RR] = variability_RR(data)
+%@TODO nno es buena idea ejecutar Pamtomkins  para cada pequeño fragmento
+%de señal.  Tiene un buffer  de inicialización. Ess decir, va a perder unos
+%cuantos de los primeros latidos. Si lo hacemos así  y las ventanas son
+%pocas vamos a tener  demasiados latidos perdidos. Habría que pasarle a
+%IndexCalculation  el resultado de la detección de latidos de todo el
+%registro, para que coja los latidos que le toca.
     [qrs,varargout] = pantompkins_qrs(data,330,logical(0));
     len = length(qrs);
     vector_RR= qrs(2:len)-qrs(1:(len-1));
+    % %@TODO  los datos que obtenemos aquí sson muy diferentes a los rangos
+    % indicados en el artículo porque nosotros estamos usando muestras de
+    % la señal  como intervalos RR,, y no tiempo.  La verdad es que no me
+    % queda claro si los intervalos deberían estar en milisegundos  o
+    % segundos. Pero aquí ya es fácil cambiar  multiplicando por 1000.
+    % Pero hay que pasar de muestras a segundos.
     var_RR = std(vector_RR)/mean(vector_RR);
 end
 
@@ -32,5 +48,9 @@ function[bas_power] = baseline_drift(data)
     freq2 = [0,40];
     p1 = bandpower(data,330,freq1);
     p2 = bandpower(data,330,freq2);
+    %%@TODO  este índice sale negativo cuando debería ser positivo.  El problema es
+    %que estamos usando  entteros comunidades arbitrarias para el cálculo
+    %de la potencia en las bandas,  y no las   unidades reales
+    %(milivoltios)  del electrocardiograma.
     bas_power = (1-p1)/p2;
 end 
