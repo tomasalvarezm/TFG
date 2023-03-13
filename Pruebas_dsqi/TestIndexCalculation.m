48003b84337d457ada725e7b859b9d9c31501dfe
agarciagrech <agarciagrech@gmail.com>
2023-03-13 16:21:46 +0100

    Tests

--- Pruebas_dsqi/TestIndexCalculation.m
+++ Pruebas_dsqi/TestIndexCalculation.m
@@ -1,22 +1,10 @@
 
-%[kSQI_01_vector,sSQI_01_vector, pSQI_01_vector, cSQI_01_vector, basSQI_01_vector,dSQI_01_vector,gmean_vector] = IndexForSignalWindows("Tela1_Escaleras.txt");
-%[kSQI_01_vector,sSQI_01_vector, pSQI_01_vector, cSQI_01_vector, basSQI_01_vector,dSQI_01_vector,geometricMean_vector] = IndexForSignalWindows("ECGNormal.txt");
 
 
 %for complete signal
-[kSQI_01,sSQI_01, pSQI_01, SQI_rel_powerLine_01,cSQI_01, basSQI_01,dSQI_01,geometricMean,averageIndex] = IndexForCompleteSignal("ECGNormal.txt");
-%[kSQI_01,sSQI_01, pSQI_01, cSQI_01, basSQI_01,dSQI_01,geometricMean] = IndexForCompleteSignal("ECGRuido.txt");
-%[kSQI_01,sSQI_01, pSQI_01, cSQI_01, basSQI_01,dSQI_01,geometricMean] = IndexForCompleteSignal("ECGRuido2.txt");
-%[kSQI_01,sSQI_01, pSQI_01, cSQI_01, basSQI_01,dSQI_01,geometricMean] = IndexForCompleteSignal("ECGRuido3.txt");
-%[kSQI_01,sSQI_01, pSQI_01, cSQI_01, basSQI_01,dSQI_01,geometricMean] = IndexForCompleteSignal("ECGRuidoNormal.txt");
-%[kSQI_01,sSQI_01, pSQI_01, cSQI_01, basSQI_01,dSQI_01,geometricMean] = IndexForCompleteSignal("Tela1_Escaleras.txt");
+[kSQI_01,sSQI_01, pSQI_01, SQI_rel_powerLine_01,cSQI_01, basSQI_01,dSQI_01,geometricMean,averageIndex] = IndexForCompleteSignal("1469157_setB.txt");
+
 
 %for windows of 10s
 
 %[kSQI_01_vector,sSQI_01_vector, pSQI_01_vector, rel_powerLine01_vector,cSQI_01_vector, basSQI_01_vector,dSQI_01_vector,geometricMean_vector,averageGeometricMean] = IndexForSignalWindows("1002603_unacceptable.txt");
-%[kSQI_01_vector,sSQI_01_vector, pSQI_01_vector, cSQI_01_vector, basSQI_01_vector,dSQI_01_vector,geometricMean_vector] = IndexForSignalWindows("ECGRuido.txt");
-%[kSQI_01_vector,sSQI_01_vector, pSQI_01_vector, cSQI_01_vector, basSQI_01_vector,dSQI_01_vector,geometricMean_vector] = IndexForSignalWindows("ECGRuido2.txt");
-%[kSQI_01_vector,sSQI_01_vector, pSQI_01_vector, cSQI_01_vector, basSQI_01_vector,dSQI_01_vector,geometricMean_vector] = IndexForSignalWindows("ECGRuido3.txt");
-%[kSQI_01_vector,sSQI_01_vector, pSQI_01_vector, cSQI_01_vector, basSQI_01_vector,dSQI_01_vector,geometricMean_vector] = IndexForSignalWindows("ECGRuidoNormal.txt");
-%[kSQI_01_vector,sSQI_01_vector, pSQI_01_vector, cSQI_01_vector, basSQI_01_vector,dSQI_01_vector,geometricMean_vector] = IndexForSignalWindows("Tela1_Escaleras.txt");
-
