a5df1addfccdb823e4cef7692cf03cbe3708973a
agarciagrech <agarciagrech@gmail.com>
2023-04-10 16:26:27 +0200

    Tests

--- Pruebas_dsqi/ANOVAAnalysis.m
+++ Pruebas_dsqi/ANOVAAnalysis.m
@@ -49,7 +49,9 @@ data_escaleras_T3_pantalon = data_T3_pantalon(1*60*1000+1:9*60*1000);
 data_trabajando_T3_pantalon = data_T3_pantalon(11*60*1000+1:19*60*1000);
 data_andando_T3_pantalon = data_T3_pantalon(21*60*1000+1:29*60*1000);
 
-%registros trabajando 
+%[kSQI_01_v,sSQI_01_v, pSQI_01_v, SQI_rel_powerLine_01_v,cSQI_01_v, basSQI_01_v,dSQI_01_v,geometricMean_V,averageGeometricMean] = IndexForSignalWindows(data_trabajando_T1_pantalon, originalFSBitalino);
+
+registros trabajando 
 T1_trabajando = {data_trabajando_T1_top,data_trabajando_T1_camiseta,data_trabajando_T1_pantalon};
 n = length(T1_trabajando);
 indexes_trabajandoT1 = cell(1,n);
